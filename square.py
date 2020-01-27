16b0c1f2cea46cbbd254410b7a30d3ddf57f1345
ABHINAYA297 <abhinayarani225@gmail.com>
2020-01-27 19:32:30 +0530

    abhinayarani225@gmail.com

---
+++ square.py
@@ -0,0 +1,13 @@
+import turtle
+for i in range(0,25):
+    turtle.left(15)
+    turtle.forward(150)
+    turtle.left(90)
+    turtle.forward(150)
+    turtle.left(90)
+    turtle.forward(150)
+    turtle.left(90)
+    turtle.forward(150)
+    turtle.left(90)
+    
+    
